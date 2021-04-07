b3c46902775845c50c7790ac6b53b94838eedd6a
nikhildwivedi7 <nikhilsteamid@gmail.com>
2021-04-08 00:42:25 +0530

    CHEF AND DICE

--- SDICE.cpp
+++ SDICE.cpp
@@ -10,6 +10,14 @@ int solve(long long X)
         return 51;
     else if (X == 4)
         return 60;
+    else if (X == 5)
+        return 76;
+    else if (X == 6)
+        return 88;
+    else if (X == 7)
+        return 99;
+    else if (X == 8)
+        return 104;
     else
         return 0;
 }
@@ -21,15 +29,15 @@ int main()
     {
         long long N, res, levels, extras;
         cin >> N;
-        if (N <= 4)
+        if (N <= 8)
         {
             res = solve(N);
         }
         else
         {
             levels = N / 4;
-            extras = N % 4;
-            res = (9 * 4 * levels) + 24 + solve(extras) - (6 * extras);
+            extras = N - (levels - 1) * 4;
+            res = (levels - 1) * 11 * 4 + solve(extras);
         }
         cout << res << endl;
     }
